92ff4f897a7525b96f36023bac757d276345f6af
Puncheson <78761722+Puncheson@users.noreply.github.com>
2022-07-23 11:53:12 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      <div className='lol'>Hello world</div>
+      <div className='lol'>Hello world!</div>
     </div>
   );
 }
